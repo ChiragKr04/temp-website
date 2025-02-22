4c8f4ff683f9afe7a39fbef620bba117a83efe1e
ChiragKr04 <chiragkumar6921@gmail.com>
2025-02-22 23:48:44 +0530

    fix: main page for landing

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,12 @@
 import { useRoutes } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 import Home1Page from "./pages/Home1";
 import New from "./pages/New/New";
 import LawFirmLanding from "./pages/New2/New2";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "*", element: <NotFound /> },
+    { path: "/", element: <LawFirmLanding /> },
+    { path: "*", element: <LawFirmLanding /> },
     { path: "home1", element: <Home1Page /> },
     { path: "new", element: <New /> },
     { path: "new2", element: <LawFirmLanding /> },
